39d0d227664b99763f61f373fd460c81b4c73ec6
sumit <sumitkolipyaka99@gmail.com>
2023-01-18 23:14:41 +0530

    done with day2

---
+++ gk_taste/src/components/Pages/OurFood.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OurFood = () => {
+  return (
+    <div>
+       <h2> this are all the food items </h2>
+    </div>
+  )
+}
+
+export default OurFood
---
+++ gk_taste/src/components/Pages/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const about = () => {
+  return (
+    <div>
+        about the page
+    </div>
+  )
+}
